50b00dc50ea335ad657a487df717a09a767d9695
CindyRavell <79164724+CindyRavell@users.noreply.github.com>
2021-04-20 03:12:15 -0500

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,12 +1,12 @@
-
-
 const fs = require('fs');
-const readline = require('readline');
-const rl = readline.createInterface(process.stdin, process.stdout);
+const rl = require('readline');
+const readline = rl.createInterface(process.stdin, process.stdout);
+const path = require('path');
+
 
 let ruta = process.cwd();
 console.log(process.cwd());
-//console.log(process.argv[2]===undefined);
+let count = 0
 
 if(process.argv[2]!==undefined){
          ruta = './'+ process.argv[2];
@@ -14,52 +14,68 @@ if(process.argv[2]!==undefined){
         throw new Error('no especificó carpeta')
 }
 
-//async
+//async question
+function question(query) {
+    return new Promise(resolve => {
+        readline.question(query, resolve);
+    })
+}
+
+async function validation(){
+    const answer = await question("this folder is not empty, Do you want to continue? [yes]/no: ");
+    readline.close();
+    return answer
+}
+
+
+async function main(){
+    let filenames = fs.readdirSync(ruta)
+    if(filenames.length>0){
+        const answer = await validation()
+        if(answer === 'yes'){
+            remove(ruta)
+        }else{
+            console.log('nosh vemosh');
+        }        
+    }else{
+        fs.rmdirSync(ruta)
+    }
+}
 
 function remove(ruta){
-        //SABBER SI LA RUTA ES UN DIRECTORIO
-        if(fs.lstatSync(ruta).isDirectory()){
-                        console.log("yes1", ruta)
-                      //CARPETAS VACIAS
-                      if ( fs.readdirSync(ruta).length === 0) {
-                                console.log("yes2")
-                                fs.rmdir(ruta, (error) => {
-                                        if (error) {
-                                                console.log(error);
-                                        }
-                                        else {
-                                        console.log("Empty Directories Deleted!");
-                                        }
-                                });
 
+    //contenido de la ruta
+    let filenames = fs.readdirSync(ruta)
+    //const filestat = fs.lstatSync(ruta); //investigar
 
-                        }
-                        //CARPETA NO VACÍA
-                        else{
-                          fs.readdir(ruta,function (err,archivos){
-                                if(err){
-                                        console.log(err)
-                                        return;
-                                }
-                                for(let i of archivos){
-                                        //PARA CADA ARCHIVO QUE SE ENCUENTRE EN LA RUTA PADRE
-                                        const rutaDirectory = ruta +"/"+i
-                                        remove(rutaDirectory)
-					console.log(rutaDirectory)
-					
-                                }
-				
-	                        			
-
-                          })
-			
-
-			         
-                        }
+    //CARPETA NO VACÍA
+    if( filenames.length > 0){
+    
+            for(let i of filenames){
+                const rutaDirectory = path.join(ruta ,i)
+                //console.log(rutaDirectory);
+                //Eliminar los que no son directorios
+                if(!fs.statSync(rutaDirectory).isDirectory()){
+                    count = count +1
+                    fs.unlinkSync(rutaDirectory)
+                }else{
+                //PARA CADA ARCHIVO QUE SE ENCUENTRE EN LA RUTA PADRE
+                remove(rutaDirectory)
+                }
+            }
+        
+            // actualizar para eliminar las carpetas padres
+            filenames = fs.readdirSync(ruta)
+        }
+    
+        //CARPETAS VACIAS 
+        if ( filenames.length === 0) {
+                fs.rmdirSync(ruta)
+                count = count + 1
 
         }
 
-
 }
+console.log('total de archivos eliminados', count)
 
-remove(ruta);
+main()
